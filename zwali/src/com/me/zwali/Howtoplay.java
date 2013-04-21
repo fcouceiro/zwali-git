3ed185ddc9204a3f30a04dd51c924bb099d7237a
fcouceiro <couceiro.f@gmail.com>
2013-04-21 13:52:42 +0100

    Cutscene class added to API - check how to play menu

--- zwali/src/com/me/zwali/Howtoplay.java
+++ zwali/src/com/me/zwali/Howtoplay.java
@@ -7,192 +7,45 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.ruthlessgames.api.CutScene;
+import com.ruthlessgames.api.Pane;
+import com.ruthlessgames.api.StylesManager;
 
 
 
-public class Howtoplay implements Screen
+public class Howtoplay extends CutScene
 {
-	private List<Sprite> Pages = new ArrayList<Sprite>(10);
-	Sprite pageIM;
-	Sprite btnNextIM, btnPrevIM, btnNext_hIM, btnPrev_hIM, btnmainMenuIM, btnmainMenu_hIM;
-	Vector nextPos,prevPos, mainMenuPos, nextSize, prevSize, mainMenuSize;
-	private int mx,my,timeclick,timerclick;
-	public int curPage;
-	SpriteBatch batch = Conceito.batch;
-	Conceito MainGame;
 	
-
-	
-	public Howtoplay(Conceito main)
+	Howtoplay(Conceito main)
 	{
-		MainGame = main;
-		Pages.add(Textures.page1);
-		Pages.add(Textures.page2);
-		Pages.add(Textures.page3);
-		Pages.add(Textures.page4);
-		Pages.add(Textures.page5);
-		Pages.add(Textures.page6);
-		
-		this.curPage = 0;
-		this.timeclick = 0;
-		this.timerclick = 15;
-		
-		btnNextIM = Textures.btnNext;
-		btnNext_hIM = Textures.btnNext_h;
-		btnPrevIM = Textures.btnPrev;
-		btnPrev_hIM = Textures.btnPrev_h;
-		btnmainMenuIM = Textures.btnmainMenu;
-		btnmainMenu_hIM = Textures.btnmainMenu_h;
-		
-		
-		nextPos = new Vector(632,500);
-		nextSize = new Vector(btnNextIM.getWidth(),btnNextIM.getHeight());
-		prevPos = new Vector(90,500);
-		prevSize = new Vector(btnPrevIM.getWidth(),btnPrevIM.getHeight());
-		mainMenuPos = new Vector(330, 520);
-		mainMenuSize = new Vector(btnmainMenuIM.getWidth(),btnmainMenuIM.getHeight());
-	}
+		super(Conceito.batch,main.questsScreen,"assets/cutscenes/teste.xml");
+		//this.pop_panes(); //create panes on current cut scene
+	}	
 	
-	
-	
-	public boolean update()
+	private void pop_panes()
 	{
+		Pane hello_label = new Pane(60*4);
+		Label test = new Label("Hello",StylesManager.arial15);
+		test.setPosition(70, 70);
+		test.addAction(Actions.fadeOut(2));
+		Label test_fade = new Label("Fade in de 20",StylesManager.arial15);
+		test_fade.setPosition(200, 70);
+		test_fade.getColor().a = 0f;
+		test_fade.addAction(Actions.sequence(Actions.fadeIn(1), Actions.delay(1), Actions.fadeOut(1)));
+		hello_label.add_Actor(test);
+		hello_label.add_Actor(test_fade);
+		addPane(hello_label);
 		
-		mx= Gdx.input.getX();
-		my= Gdx.input.getY();
+		Pane teste = new Pane(60*4);
+		TextButton t = new TextButton("x",StylesManager.btnLock);
+		t.addAction(Actions.fadeOut(3));
+		teste.add_Actor(t);
+		addPane(teste);
 		
-		timeclick++;
-		pageIM = Pages.get(curPage);
-		
-		this.draw();
-		
-		if(Gdx.input.isTouched())
-		{
-			if (this.timeclick >= this.timerclick) 
-			{
-				this.timeclick = 0;
-				if (mx>nextPos.x && mx<(nextPos.x + nextSize.x - 50) && my>nextPos.y && my<(nextPos.y + nextSize.y - 100)) {
-					this.setCurPage(curPage + 1);
-				} else if (mx>prevPos.x && mx<(prevPos.x + prevSize.x-5) && my>prevPos.y && my<(prevPos.y + prevSize.y - 100)) {
-					this.setCurPage(curPage - 1);
-				} else if (mx>mainMenuPos.x && mx<(mainMenuPos.x + mainMenuSize.x - 73) && my>mainMenuPos.y +10  && my<(mainMenuPos.y + mainMenuSize.y - 19))
-					MainGame.setScreen(MainGame.mainmenu);
-			}
-			
-		}
-		return true;
-	}
-	
-	public void setCurPage(int n)
-	{
-		int pageIndex = n;
-		if(pageIndex == -1) pageIndex = Pages.size() - 1;
-		else if(pageIndex == Pages.size()) pageIndex = 0;
-		
-		if(pageIndex < Pages.size() && pageIndex >= 0)
-		{
-			this.curPage = pageIndex;
-		}
-		else System.out.println("---Erro na page index!---");
-	}
-	
-	private void drawNext()
-	{
-
-				Sprite a = null;
-				if(mx>nextPos.x && mx<(nextPos.x + nextSize.x ) && my>nextPos.y && my<(nextPos.y + nextSize.y )) a = btnNext_hIM;
-				else a = btnNextIM;
-				a.setPosition((float)nextPos.x,470 - (float)nextPos.y);
-				a.setSize((float)nextSize.x, (float)nextSize.y);
-				a.draw(batch);
-				
-	}
-	
-	private void drawPrev()
-	{
-
-				Sprite a = null;
-				if(mx>prevPos.x && mx<(prevPos.x + prevSize.x-5) &&  my>prevPos.y && my<(prevPos.y + prevSize.y - 100)) a=btnPrev_hIM;
-				else a=btnPrevIM;
-				a.setPosition((float)prevPos.x, 470 - (float)prevPos.y);
-				a.setSize((int)prevSize.x,(int)prevSize.y);
-				a.draw(batch);
-				
-	}
-	private void drawmainMenu()
-	{
-				Sprite a = null;
-				if(mx>mainMenuPos.x && mx<(mainMenuPos.x + mainMenuSize.x - 73) && my>mainMenuPos.y +10  && my<(mainMenuPos.y + mainMenuSize.y - 19 )) 
-					a = btnmainMenu_hIM;
-				else 
-					a = btnmainMenuIM;
-				a.setPosition((float)mainMenuPos.x, 520 - (float)mainMenuPos.y);
-				a.setSize((int)mainMenuSize.x,(int)mainMenuSize.y);
-				a.draw(batch);
-	}
-	
-	private void draw()
-	{
-
-		this.drawBG();
-		this.drawmainMenu();
-		this.drawNext();
-		this.drawPrev();
-
-	}
-	
-	private void drawBG()
-	{
-		//Page
-		pageIM.setPosition(0,0);
-		pageIM.setSize(800,600);
-		pageIM.draw(batch);
-
-	}
-
-	@Override
-	public void render(float delta) {
-		// TODO Auto-generated method stub
-		Conceito.batch.begin();
-		this.update();
-		Conceito.batch.end();
-	}
-
-	@Override
-	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-	
 	
 }
