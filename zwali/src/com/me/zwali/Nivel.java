b852bb985cea5138115358d140eba5046f797dd4
Lactor <machado007@gmail.com>
2013-06-05 16:26:20 +0100

    inicio de nível

---
+++ zwali/src/com/me/zwali/Nivel.java
@@ -0,0 +1,32 @@
+package com.me.zwali;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Nivel {
+	
+	
+	List <Cenario> levels = new ArrayList<Cenario>(10);
+	Sprite background;
+	int openedlevel;
+	
+	public Nivel(int type){
+		
+		switch(type){
+		
+		case 0:
+			for(int i = 0; i<4; i++)
+		break;
+		
+		case 1:
+			
+		break;
+		
+		}
+		
+		
+	}
+	
+}
